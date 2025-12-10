096e1e61ee455398c1802ce4019c6996b7058d27
dimitriossyngiridis <sigiridis.dimitris9@gmail.com>
2025-12-10 23:45:58 +0200

    test

--- lib/board.php
+++ lib/board.php
@@ -6,10 +6,10 @@ function show_board() {
 	$st = $mysqli->prepare($sql); //Αυτό βελτιώνει την ασφάλεια και την απόδοση.
 	$st->execute();
 	$res = $st->get_result();
-	header('Content-type: application/json'); //Στέλνει μια κεφαλίδα HTTP στον browser (ή στον πελάτη) ενημερώνοντάς τον ότι τα δεδομένα που ακολουθούν είναι σε μορφή JSON και όχι σε απλή HTML.
+	header('Content-type: application/json'); //επιστρεφουμε JSON
 	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 }
-
+// reset board function
 function reset_board() {
 	global $mysqli;
 	
